ac29bc82e02d4f034d2c26a083012e69ac60bb1f
debarunsengupta <debarunsengupta2512@gmail.com>
2020-05-15 13:01:32 +0530

    Update eventNotifier.js

--- src/modules/my/eventNotifier/eventNotifier.js
+++ src/modules/my/eventNotifier/eventNotifier.js
@@ -1,14 +1,4 @@
-/**
- * @File Name          : EventNotifier.js
- * @Description        : Shows Notification when an event is published on the specified channel
- * @Group              : Playground
- * @Author             : Debarun Sengupta
- * @Last Modified By   : Debarun Sengupta
- * @Last Modified On   : 5/15/2020, 10:00:00 AM
- * @Modification Log   :
- * Ver       Date            Author      		    Modification
- * 1.0      5/15/2020        Initial Version
- **/
+
 
 import { LightningElement } from 'lwc';
 
